5e5621ac6812a1e2887b188bc7ded8377a69da67
unknown <homeshwaryellenti@gmail.com>
2022-07-10 21:40:09 +0530

    database tables complete

---
+++ spotify/app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
+from django.db.models import Sum, Avg
+# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    dateofbirth = models.DateField(blank = True, null=True)
+    bio = models.TextField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    @property 
+    def artist_avg(self):
+        queryset = self.song_set.all().filter(rating__gt=0).aggregate(Avg('rating'))
+        return int(queryset['rating__avg'])
+   
+
+
+class Song(models.Model):
+    artists = models.ManyToManyField(Artist)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=255)
+    date = models.DateField(blank = True, null=True)
+    rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)], default=0)
+
+    def __str__(self):
+        return self.title
